5e82ca27fa96fd0ea50b897298b9edef3e9eff15
Nithin Uppalapati <ee18b035@smail.iitm.ac.in>
2021-05-30 18:20:04 +0530

    updated tokeniser to stem symbols marks/symbols

--- project_nlp/tokenization.py
+++ project_nlp/tokenization.py
@@ -52,9 +52,30 @@ class Tokenization():
 			A list of lists where each sub-list is a sequence of tokens
 		"""
 		l = []
+
+		trim_set = {"/", ".", "!", "?", "'", '"', "_", "*", "(", ")", "{", "}", "[", "]", "<", ">"} 
 		for i in text:
+			temp_lst = []
 			w = word_tokenize(i)
-			l.append(w)
+			for j in w:
+				if not(j[0] in trim_set) and not(j[-1] in trim_set):
+					temp_lst.append(j)
+
+				elif (j[0] in trim_set) and not(j[-1] in trim_set):
+					final_trimmed_word = j[1:]
+					temp_lst.append(final_trimmed_word)
+
+				elif (j[-1] in trim_set) and not(j[0] in trim_set):
+					final_trimmed_word = j[:-1]
+					temp_lst.append(final_trimmed_word)
+
+				else:
+					j_new = j[1:]
+					final_trimmed_word = j_new[:-1]
+					temp_lst.append(final_trimmed_word)
+
+			l.append(temp_lst)
+			# l.append(w)
 			
 		tokenizedText=l
 		# print(tokenizedText)
