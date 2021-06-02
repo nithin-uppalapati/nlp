40f3d9941a26d9944162c26545ec1681ee959f95
Nithin Uppalapati <ee18b035@smail.iitm.ac.in>
2021-06-02 10:31:00 +0530

    added colab_folder, and otther updates

---
+++ project_nlp_colab/inflectionReduction.py
@@ -0,0 +1,41 @@
+from util import *
+# from nltk.stem import PorterStemmer, WordNetLemmatizer
+# Add your import statements here
+
+
+
+
+class InflectionReduction:
+
+	def reduce(self, text):
+		"""
+		Stemming/Lemmatization
+
+		Parameters
+		----------
+		arg1 : list
+			A list of lists where each sub-list a sequence of tokens
+			representing a sentence
+
+		Returns
+		-------
+		list
+			A list of lists where each sub-list is a sequence of
+			stemmed/lemmatized tokens representing a sentence
+		"""
+
+		# reducedText = None
+
+		#Fill in code here
+		# Performing lemmatization to reduce inflection.
+		wrntl=WordNetLemmatizer()
+		# ps=PorterStemmer()
+
+		temp=text.copy()
+		for i in range(0,len(text)):
+			for j in range(0,len(text[i])):
+				temp[i][j]=wrntl.lemmatize(text[i][j])
+		
+		reducedText = temp.copy()
+		
+		return reducedText
---
+++ project_nlp_colab/tokenization.py
@@ -0,0 +1,82 @@
+from util import *
+# from nltk.tokenize import word_tokenize 
+# Add your import statements here
+
+
+
+
+class Tokenization():
+
+	def naive(self, text):
+		"""
+		Tokenization using a Naive Approach
+
+		Parameters
+		----------
+		arg1 : list
+			A list of strings where each string is a single sentence
+
+		Returns
+		-------
+		list
+			A list of lists where each sub-list is a sequence of tokens
+		"""
+# ---------------------------------------
+		tokenizedText = None
+
+		#Fill in code here
+		lst=[]
+		for i in text:
+			i.replace("\n"," ")
+			lst.append(i.split(" "))
+		
+		tokenizedText=lst
+		return tokenizedText
+# ---------------------------------------
+
+
+
+
+	def pennTreeBank(self, text):
+		"""
+		Tokenization using the Penn Tree Bank Tokenizer
+
+		Parameters
+		----------
+		arg1 : list
+			A list of strings where each string is a single sentence
+
+		Returns
+		-------
+		list
+			A list of lists where each sub-list is a sequence of tokens
+		"""
+		l = []
+
+		trim_set = {"/", ".", "!", "?", "'", '"', "_", "*", "(", ")", "{", "}", "[", "]", "<", ">"} 
+		for i in text:
+			temp_lst = []
+			w = word_tokenize(i)
+			for j in w:
+				if not(j[0] in trim_set) and not(j[-1] in trim_set):
+					temp_lst.append(j)
+
+				elif (j[0] in trim_set) and not(j[-1] in trim_set):
+					final_trimmed_word = j[1:]
+					temp_lst.append(final_trimmed_word)
+
+				elif (j[-1] in trim_set) and not(j[0] in trim_set):
+					final_trimmed_word = j[:-1]
+					temp_lst.append(final_trimmed_word)
+
+				else:
+					j_new = j[1:]
+					final_trimmed_word = j_new[:-1]
+					temp_lst.append(final_trimmed_word)
+
+			l.append(temp_lst)
+			# l.append(w)
+			
+		tokenizedText=l
+		# print(tokenizedText)
+		return tokenizedText
